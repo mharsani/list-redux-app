9188adddf2893732af5b5a2b50c6a1956d715c1e
Matheus Harsani <Matheus.Harsani@keyruscorp.key>
2018-12-10 09:14:32 -0200

    add new logic for component filter

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import {connect} from 'react-redux'
+import {setVisibilityFilter} from '../../redux-flow/reducers/visibility-filter/action-creators'
 
-export const Filter = () =>  (
+export const Filter = ({activeFilter, handleFilter}) =>  (
     <div>
-        <a>Ativo</a>
-        <a>Fechado</a>
-        <a>Todos</a>
+       {filterItems.map((item)=> {
+           if(item.action === activeFilter) {
+               return <span href='' key={item.action} style={{marginRight:10}}>{item.label}</span>
+           }
+           return <a href='' key={item.action} style={{marginRight:10}} onClick={handleFilter(item.action)}>{item.label}</a>
+       })}
     </div>
 )
+const mapDispacthToProps  = (dispatch) => ({
+   handleFilter: (filter) => (e) => {
+       e.preventDefault()
+       dispatch(setVisibilityFilter(filter))
+   }
+})
 
-export default Filter
+const filterItems = [{label: 'Todos', action: 'SHOW_ALL'}, {label: 'Finalizados', action: 'SHOW_COMPLETED'},{label: 'A fazer', action: 'SHOW_ACTIVE'}]
+
+const mapStateToProps = (state) => ({
+ activeFilter: state.filter
+})
+
+export default connect(mapStateToProps, mapDispacthToProps)(Filter)
 
